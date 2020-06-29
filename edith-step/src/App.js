7498ec67c38f3839e7a8a264c9246813bce8cfa0
Prashant Neupane <prashantneupane8848@gmail.com>
2020-06-29 18:39:39 +0000

    Add navbar at the top

--- edith-step/src/App.js
+++ edith-step/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import TopNavbar from './components/TopNavbar';
+
 import logo from './logo.svg';
 import "./App.css";
 
@@ -16,6 +18,7 @@ class App extends Component {
   render() {
  return (
       <div className="App">
+        <TopNavbar />
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
---
+++ edith-step/src/components/TopNavbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+class TopNavbar extends React.Component {
+
+  render() {
+    return (
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="/">Edith</Navbar.Brand>
+        <Nav className="ml-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="/">Log In</Nav.Link>
+        </Nav>
+      </Navbar>
+    );
+  }
+}
+
+export default TopNavbar; 
