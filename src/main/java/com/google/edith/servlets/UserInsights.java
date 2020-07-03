c2e7026ffa0fce17f5b911230c10d1a0c279f411
Malachi Brewer <malachibre@gmail.com>
2020-07-03 07:47:43 +0000

    Fix aggregateUserData() and refactor code

--- src/main/java/com/google/edith/servlets/UserInsights.java
+++ src/main/java/com/google/edith/servlets/UserInsights.java
@@ -5,23 +5,19 @@ import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.EntityNotFoundException;
 import com.google.appengine.api.datastore.FetchOptions;
-import com.google.appengine.api.datastore.Query.Filter;
-import com.google.appengine.api.datastore.Query.FilterOperator;
-import com.google.appengine.api.datastore.Query.FilterPredicate;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
-import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.appengine.api.datastore.Query.Filter;
+import com.google.appengine.api.datastore.Query.FilterOperator;
+import com.google.appengine.api.datastore.Query.FilterPredicate;
 import com.google.gson.Gson;
-import java.io.IOException;
-import java.time.temporal.ChronoUnit;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 import java.time.LocalDate;
-import java.util.stream.Collectors;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
+import java.util.List;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
@@ -33,54 +29,62 @@ import java.util.Map;
  */
 public final class UserInsights {
 
+  private static String userId;
+
   private static final DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();  
   private static final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
   private static final Gson gson = new Gson();
-  private static String userId;
 
   /** Assumes dates are in the form yyyy-MM-dd */
-  private static Comparator<Key> SORT_BY_DATE = (Key item1, Key item2) -> {
+  private static final Comparator<Key> SORT_BY_DATE = (Key item1, Key item2) -> {
       try {
         return ((String) (datastore.get(item1).getProperty("date")))
             .compareTo((String) (datastore.get(item2).getProperty("date")));
-      } catch(EntityNotFoundException e)  {
+      } catch(EntityNotFoundException | NullPointerException e )  {
         return 0;
       }
   };
-  
   
   public UserInsights(String userId) {
     this.userId = userId;
   }
 
-  /** This should only be called each time a new user makes an accout. */
-  public void createUserStats() { 
-    List<Key> items = new ArrayList<>();
-
-    Entity newUserStats = new Entity("UserStats");
-
-    newUserStats.setProperty("userId", userId);
-    newUserStats.setProperty("items", items);
-
-    datastore.put(newUserStats);
-  }
-
-  /** 
-   * Updates the items list in the UserStats Entity in datastore
-   * corresponding to this user.
-   * @param items - The list of items to be added to the current item list.
+  /**
+   * Finds the UserStats entity corresponding to {@code userId} in datastore.
+   * @return UserStats entity for this user
    */
-  public void updateUserStats(List<Key> newItems) {
-    Filter idFilter = new FilterPredicate("userId", 
+  private Entity retreiveUserStats() {
+    Filter idFilter = new FilterPredicate("UserId", 
                                 FilterOperator.EQUAL,
                                 userId);
     Query query = new Query("UserStats").setFilter(idFilter);
     PreparedQuery results = datastore.prepare(query);
-    Entity userStats = results.asList(FetchOptions.Builder.withLimit(10))
-                              .get(0);
+    return results.asList(FetchOptions.Builder.withLimit(10)).get(0);
+  }
 
+  /** This should only be called each time a new user makes an accout. */
+  public void createUserStats() { 
+     List<Key> items = new ArrayList<>();
+
+     Entity userStats = new Entity("UserStats");
+     userStats.setProperty("UserId", userId);
+     userStats.setProperty("Items", items);
+     datastore.put(userStats);
+  }
+
+  /** 
+   * Updates the Items list in the UserStats Entity in datastore
+   * corresponding to this user.
+   * @param items - Non-null list of item Keys to be added to the current Item list.
+   */
+  public void updateUserStats(List<Key> newItems) {
+    Entity userStats = retreiveUserStats();    
     List<Key> items = (List<Key>) userStats.getProperty("Items");
-    items.addAll(newItems);
+    if (items == null) {
+      items = newItems;
+    } else {
+      items.addAll(newItems);
+    }
     userStats.setProperty("Items", items);
     datastore.put(userStats);
 
@@ -89,23 +93,16 @@ public final class UserInsights {
    /** 
     * Copmiles the spending using the Item list found in this user's
     * UserStats Entity in datastore.
-    * TODO: Allow for various time periods (only calculates weekly total now).
+    * TODO: Allow for various time periods (only calculates weekly aggregate now).
     * @return A map relating a time period to the spending in that time period.
     */
-  public Map<String, Integer> aggregateUserData() { 
-    Filter idFilter = new FilterPredicate("userId", 
-                                FilterOperator.EQUAL,
-                                userId);
-    Query query = new Query("UserStats").setFilter(idFilter);                                   
-    PreparedQuery results = datastore.prepare(query);
-    Entity userStats = results.asList(FetchOptions.Builder.withLimit(10))
-                              .get(0);
+  public Map<String, Double> aggregateUserData() { 
+    Entity userStats = retreiveUserStats();
     List<Key> items = (List<Key>) userStats.getProperty("Items");
 
     items.sort(SORT_BY_DATE);
 
     return calculateWeeklyTotal(items);
-
   }
 
   /**
@@ -115,53 +112,52 @@ public final class UserInsights {
    * @return Creates a map with keys for each ending day of a weekly period and  
    *         values for the total spending during that period. 
    */
-  public Map<String, Integer> calculateWeeklyTotal(List<Key> items)  {
-    
-    Map<String, Integer> weeklyTotals = new HashMap<String, Integer>();
-    LocalDate currentStartOfWeek;
+  public Map<String, Double> calculateWeeklyTotal(List<Key> items)  {
+    Map<String, Double> weeklyTotals = new HashMap<String, Double>();
+    LocalDate currentEndOfWeek;
     try {
-      currentStartOfWeek = LocalDate.parse((String) datastore.get(items.get(0))
-                                            .getProperty("date"), dateFormatter);
+      currentEndOfWeek = getEndOfWeek(LocalDate.parse((String) datastore.get(items.get(0))
+                                          .getProperty("date"), dateFormatter));
     } catch(EntityNotFoundException e) {
-      throw new IllegalArgumentException();
+      System.err.println("Error: Entity could not be located");
+      return null;
     }
-    int weeklyTotal = 0;
+    double weeklyTotal = 0;
     Entity itemEntity;
     for(Key item : items) {
       try {
         itemEntity = datastore.get(item);
-      } catch(EntityNotFoundException e) {
-        throw new IllegalArgumentException();
-      }
-      LocalDate itemDate = LocalDate.parse((String) itemEntity.getProperty("date"),
-                                           dateFormatter);
-      if(ChronoUnit.DAYS.between(currentStartOfWeek, itemDate) > 6) {
-        currentStartOfWeek = getStartOfWeek(itemDate);
-        weeklyTotals.put(currentStartOfWeek.toString(), weeklyTotal);
-        weeklyTotal = 0;
-      }
+        LocalDate itemDate = LocalDate.parse((String) datastore.get(item).getProperty("date"),
+                                             dateFormatter);
+        if(ChronoUnit.DAYS.between(itemDate, currentEndOfWeek) < 0) {
+          weeklyTotals.put(currentEndOfWeek.toString(), weeklyTotal);
+          currentEndOfWeek = getEndOfWeek(itemDate);
+          weeklyTotal = 0;
+        }
 
-      weeklyTotal += ((int) itemEntity.getProperty("price")) * 
-                         ((int) itemEntity.getProperty("quantity")); 
-
+        weeklyTotal += ((double) itemEntity.getProperty("price")) * 
+                           ((long) itemEntity.getProperty("quantity"));
+    
+      } catch (EntityNotFoundException e) {
+        System.err.println("Error: Entity could not be found");
+      } 
     }
+    
+    weeklyTotals.put(currentEndOfWeek.toString(), weeklyTotal);
 
     return weeklyTotals;
   }
 
   /**
-   * Finds the Date for the first day of the week that {@code itemDate} is in 
-   * (weeks start on Sunday and end on Saturday).
-   * @param itemDate - Uses the integer day value of the week from this 
-   *                   paramter to calculate the start of its week.
-   * @return the LocalDate representing the start of the week {@code itemDate}
+   * Finds the Date for the last day of the week that {@code itemDate} is in 
+   * (weeks start on Monday and end on Sunday).
+   * @param itemDate - Uses the int day value of the week from this 
+   *                   paramter to calculate the end of its week.
+   * @return the LocalDate representing the end of the week {@code itemDate}
    *         is in.
    */
-  public LocalDate getStartOfWeek(LocalDate itemDate) {
+  public LocalDate getEndOfWeek(LocalDate itemDate) {
     int currentDayOfWeek = itemDate.getDayOfWeek().getValue();
-    return itemDate.minusDays(currentDayOfWeek);
-  }
-
-   
-  
+    return itemDate.plusDays(7 - currentDayOfWeek);
+  }  
 }
--- src/test/java/com/google/edith/UserInsightsTest.java
+++ src/test/java/com/google/edith/UserInsightsTest.java
@@ -1,40 +1,29 @@
 package com.google.sps;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-import com.google.appengine.api.datastore.FetchOptions;
-import com.google.appengine.api.datastore.Key;
-import com.google.appengine.api.datastore.KeyFactory;
-import com.google.appengine.api.datastore.EntityNotFoundException;
-import com.google.appengine.api.datastore.Query;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
-import org.junit.After;
-import org.junit.Assert;
+import com.google.sps.servlets.UserInsights;
+import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 import java.time.LocalDate;
-import org.junit.Before;
-import org.junit.Test;
 import java.util.ArrayList;
-import java.util.Map;
-import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
-import com.google.sps.servlets.UserInsights;
-
+import java.util.Map;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.junit.Test;
 
 public final class UserInsightsTest {
   private static final String USER_ID = "userId";
@@ -86,9 +75,9 @@ public final class UserInsightsTest {
   public void updateUserStatsWhenDefault() {
     // The Items property in thie UserStats entity should be equal to 
     // {@code items}
-    List<Key> items = new ArrayList<Key>();
+    List<Key> items = new ArrayList<>();
     for(int i = 0; i < 5; i++) {
-       items.add(KeyFactory.createKey("Item", "Item" + i));
+      items.add(KeyFactory.createKey("Item", "Item" + i));
     }
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     userInsights.createUserStats();
@@ -123,54 +112,50 @@ public final class UserInsightsTest {
   }
 
   @Test
-  public void aggregateData() {
-    // The Items property in thie UserStats entity should be equal to 
-    // {@code items}
-        
+  public void aggregateData() {    
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();   
     
     List<Key> items = new ArrayList<>();
+
     items.add(KeyFactory.createKey("Item", "Item1"));
     items.add(KeyFactory.createKey("Item", "Item2"));
     items.add(KeyFactory.createKey("Item", "Item3"));
     items.add(KeyFactory.createKey("Item", "Item4"));
 
+    // The week containing {@code Entity} and {@code Entity}
+    // concludes on 07-05-2020 and the total should be 17.00.
     Entity newEntity = new Entity(items.get(0));
-
     newEntity.setProperty("price", 5.00);
     newEntity.setProperty("quantity", 1);
     newEntity.setProperty("date", "2020-06-29");
     datastore.put(newEntity); 
 
     Entity newEntity2 = new Entity(items.get(1));
-
     newEntity2.setProperty("price", 6.00);
     newEntity2.setProperty("quantity", 2);
     newEntity2.setProperty("date", "2020-06-30");
-    datastore.put(newEntity2);  
+    datastore.put(newEntity2); 
 
+    // The week containing {@code Entity3} and {@code Entity4}
+    // concludes on 07-12-2020 and the total should be 53.00.
     Entity newEntity3 = new Entity(items.get(2));
-
     newEntity3.setProperty("price", 7.00);
     newEntity3.setProperty("quantity", 3);
-    newEntity3.setProperty("date", "2020-07-06"); 
+    newEntity3.setProperty("date", "2020-07-11"); 
     datastore.put(newEntity3);  
 
     Entity newEntity4 = new Entity(items.get(3));
-
     newEntity4.setProperty("price", 8.00);
     newEntity4.setProperty("quantity", 4);
-    newEntity4.setProperty("date", "2020-07-06");
+    newEntity4.setProperty("date", "2020-07-12");
     datastore.put(newEntity4);  
 
     Map<String, Double> map = new HashMap<String, Double>();
-    map.put("2020-07-04", 17.00);
-    map.put("2020-07-11", 53.00);
+    map.put("2020-07-05", 17.00);
+    map.put("2020-07-12", 53.00);
     userInsights.createUserStats();
     userInsights.updateUserStats(items);
    
     Assert.assertEquals(map, userInsights.aggregateUserData());
-   
   }
-
 }
