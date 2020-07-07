25623e320d7699958a1bd5d50da1281028bbdefa
Prashant Neupane <prashantneupane8848@gmail.com>
2020-07-07 18:43:36 +0000

    Add tests

--- edith-step/src/components/TopNavbar.js
+++ edith-step/src/components/TopNavbar.js
@@ -3,7 +3,6 @@ import React from 'react';
 import Dropdown from 'react-bootstrap/Dropdown';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 class TopNavbar extends React.Component {
   constructor(props) {
@@ -32,26 +31,26 @@ class TopNavbar extends React.Component {
 
   render() {
     return (
-      <Navbar bg='dark' variant='dark' fixed='top' expand="lg">
+      <Navbar bg='dark' variant='dark' fixed='top' expand='lg'>
         <Navbar.Brand href='/'>EDITH</Navbar.Brand>
           <Nav className='ml-auto links'>
-            <Nav.Link href='#home' className="home">HOME</Nav.Link>
-            <Nav.Link href='#features' className="features">FEATURES</Nav.Link>
+            <Nav.Link href='#home' className='home'>HOME</Nav.Link>
+            <Nav.Link href='#features' className='features'>FEATURES</Nav.Link>
             {this.state.user === null &&
-              <Nav.Link href='/login' className="login-button">LOG IN</Nav.Link>
+              <Nav.Link href='/login' className='login-button'>LOG IN</Nav.Link>
             }
             {this.state.user &&
               <>
-                <Nav.Link href='#dashboard'>DASHBOARD</Nav.Link>
-                <Dropdown>
-                  <Dropdown.Toggle variant="dark" id="dropdown-basic">
+                <Nav.Link href='#dashboard' className='dashboard'>DASHBOARD</Nav.Link>
+                <Dropdown className='dropdowns'>
+                  <Dropdown.Toggle variant='dark' id='dropdown-basic' className='dropdown-toggle'>
                     {this.state.user.email}
                   </Dropdown.Toggle>
-                  <Dropdown.Menu>
-                    <Dropdown.Item>Upload Receipt</Dropdown.Item>
-                    <Dropdown.Item>Set Nickname</Dropdown.Item>
-                    <Dropdown.Item>Invite Friends</Dropdown.Item>
-                    <Dropdown.Item href={this.state.user.logOutUrl}>Log Out</Dropdown.Item>
+                  <Dropdown.Menu className='dropdown-menu'>
+                    <Dropdown.Item className='upload-receipt'>Upload Receipt</Dropdown.Item>
+                    <Dropdown.Item className='set-nickname'>Set Nickname</Dropdown.Item>
+                    <Dropdown.Item className='invite-friends'>Invite Friends</Dropdown.Item>
+                    <Dropdown.Item href={this.state.user.logOutUrl} className='log-out'>Log Out</Dropdown.Item>
                   </Dropdown.Menu>
                 </Dropdown>
               </>
--- edith-step/src/components/TopNavbar.test.js
+++ edith-step/src/components/TopNavbar.test.js
@@ -2,41 +2,88 @@ import React from 'react';
 import { mount } from 'enzyme';
 import TopNavbar from './TopNavbar';
 
-import '../setupTests.js'
 import { enableFetchMocks } from 'jest-fetch-mock'
+import '../setupTests.js'
 
 let component;
+let userObj = {'email': 'email'}
+describe('When not logged in, Top Navigation Bar', () => {
 
-beforeEach(() => {
-  component = mount(<TopNavbar />);
-  enableFetchMocks();
-})
+  beforeEach(() => {
+    component = mount(<TopNavbar />);
+    enableFetchMocks();
+    component.setState({user: null});
+  });
 
-afterEach(() => {
-  component.unmount();
+  afterEach(() => {
+    component.unmount();
+  });
+
+  // Checks if the Navigation Bar is rendered.
+  test('renders', () => {
+    expect(component.exists()).toBe(true);
+  });
+
+  // Checks if the navigation Bar provides three different options.
+  test('has three options', () => {
+    expect(component.find('.links').length).toBe(3);
+  });
+
+  // Checks if there is Home option.
+  test('has Home option', () => {
+    expect(component.find('.home').exists()).toBe(true);
+  });
+
+  // Checks if there is Features option.
+  test('has Features option', () => {
+    expect(component.find('.features').exists()).toBe(true);
+  });
+
+  // Checks if there is Login option.
+  test('has Login option', () => {
+    expect(component.find('.login-button').exists()).toBe(true);
+  });
+
+  // Checks if there is Dashboard option.
+  test('has Dashboard section', () => {
+    expect(component.find('.dashboard').exists()).toBe(false);
+  });
+  
+  // Checks if dropdown is shown.
+  test('should not display dropdown', () => {
+    expect(component.find('.dropdowns').exists()).toBe(false);
+  });
 });
 
-// Checks if the Navigation Bar is rendered.
-test('renders', () => {
-  expect(component.exists()).toBe(true);
-});
+describe('When logged in, Top Navigation Bar', () => {
 
-// Checks if the navigation Bar provides three different options.
-test('has three options', () => {
-  expect(component.find('.links').length).toBe(3);
-});
+  beforeEach(() => {
+    component = mount(<TopNavbar />);
+    enableFetchMocks();
+    component.setState({user: userObj});
+  });
 
-// Checks if there is Home option.
-test('has Home option', () => {
-  expect(component.find('.home').exists()).toBe(true);
-});
+  afterEach(() => {
+    component.unmount();
+  });
 
-// Checks if there is Features option.
-test('has Features option', () => {
-  expect(component.find('.features').exists()).toBe(true);
-});
+  // Checks if the Navigation Bar is rendered.
+  test('renders', () => {
+    expect(component.exists()).toBe(true);
+  });
 
-// Checks if there is Login option.
-test('has Login option', () => {
-  expect(component.find('.login-button').exists()).toBe(true);
+  // Checks if there is Dashboard option.
+  test('has Dashboard section', () => {
+    expect(component.find('.dashboard').exists()).toBe(true);
+  });
+
+  // Checks if there is Login option.
+  test('does not have Login option', () => {
+    expect(component.find('.login-button').exists()).toBe(false);
+  });
+
+  // Checks if there is Dropdown menu.
+  test('has Dropdown menu', () => {
+    expect(component.find('.dropdowns').exists()).toBe(true);
+  });
 });
