645898e3a3a59680995c4a2426817c5a4081a9c3
Malachi Brewer <malachibre@gmail.com>
2020-07-09 17:33:23 +0000

    Updated createJson() test and added Item.java suggestion

--- src/main/java/com/google/edith/servlets/Item.java
+++ src/main/java/com/google/edith/servlets/Item.java
@@ -2,7 +2,9 @@ package com.google.edith.servlets;
 
  /** Used to hold the properties of an item Entity in datastore. */
 public final class Item {
-  //TODO: Add name, user, category, and receiptId fields.
+  // TODO (malachibre): Add name, user, category, and receiptId fields.
+  // TODO (malachibre): Create a builder for this class inclduing checks 
+  //                    for valid assignments.
   double price; 
   long quantity;  
   String date; 
