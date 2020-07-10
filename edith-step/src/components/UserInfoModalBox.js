db9424b834958fe9bb161e8e770459db2ec5ce65
Prashant Neupane <prashantneupane8848@gmail.com>
2020-07-10 16:42:25 +0000

    Add test validating elements in form

--- edith-step/src/components/UserInfoModalBox.js
+++ edith-step/src/components/UserInfoModalBox.js
@@ -5,6 +5,7 @@ import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 import Row from 'react-bootstrap/Row';
+
 class UserInfoModalBox extends React.Component {
   
   constructor(props) {
@@ -28,21 +29,21 @@ class UserInfoModalBox extends React.Component {
             <Row>
               <Form.Group as={Col}>
                 <Form.Label>First Name</Form.Label>
-                <Form.Control placeholder="First Name" name="first-name" required />
+                <Form.Control placeholder="First Name" className='first-name' name="first-name" required />
               </Form.Group>
               <Form.Group as={Col}>
                 <Form.Label>Last Name</Form.Label>
-                <Form.Control placeholder="Last Name" name="last-name" required />
+                <Form.Control placeholder="Last Name" className='last-name' name="last-name" required />
               </Form.Group>
             </Row>
             <Row>
               <Form.Group as={Col}>
                 <Form.Label>Username</Form.Label>
-                <Form.Control placeholder="Username" name="username" required />
+                <Form.Control placeholder="Username" className='username' name="username" required />
               </Form.Group>
               <Form.Group as={Col}>
                 <Form.Label>Favorite Store</Form.Label>
-                <Form.Control placeholder="Favorite Store" name="favorite-store" required />
+                <Form.Control placeholder="Favorite Store" className='favorite-store' name="favorite-store" required />
               </Form.Group>
             </Row>
             <Button variant="primary" type="submit" >
---
+++ edith-step/src/components/UserInfoModalBox.test.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { enableFetchMocks } from 'jest-fetch-mock'
+enableFetchMocks();
+
+import UserInfoModalBox from './UserInfoModalBox';
+
+import '../setupTests.js'
+
+let component;
+let form;
+
+describe("UserInfoModalBox must", () => {
+  
+  beforeEach(() => {
+    component = shallow(<UserInfoModalBox />);
+    form = component.find('Form');
+  });
+
+  afterEach(() => {
+    component.unmount();
+  });
+
+  it("render", () => {
+    console.log(form.debug());
+    expect(component.exists()).toBe(true);
+  });
+
+  it("have post method", () => {
+    expect(form.props().method).toBe('POST');
+  });
+
+  it("be directed to login endpoint", () => {
+    expect(form.props().method).toBe('POST');
+  });
+
+  it("have first name field", () => {
+    expect(form.find('.first-name').exists()).toBe(true);
+  });
+
+  it("have last name field", () => {
+    expect(form.find('.last-name').exists()).toBe(true);
+  });
+
+  it("have username field", () => {
+    expect(form.find('.username').exists()).toBe(true);
+  });
+
+  it("have favorite-store field", () => {
+    expect(form.find('.favorite-store').exists()).toBe(true);
+  });
+});
