c29577e04265671142e4c01b3d28bdf230ab4f4a
Livia Seibert <livia.seibert@duke.edu>
2020-07-13 18:36:16 +0000

    more data extraction

--- src/main/java/com/google/edith/ShelfDataReader.java
+++ src/main/java/com/google/edith/ShelfDataReader.java
@@ -1,30 +1,72 @@
 package com.google.edith;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
- 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.StreamSupport;
+import java.util.stream.Collectors;
+
+import java.net.URL;
+
+/** Processes data file of product expiration information
+  * to populate future user grocery lists. */
 public class ShelfDataReader {
 
-  public void readFile(String itemName) {
+  /** Finds the specified product in the file . */
+  public String readFile(String itemName) {
     URL jsonresource = getClass().getClassLoader().getResource("foodkeeper.json");
     File shelfLifeData = new File(jsonresource.getFile());
 
     JsonParser jsonParser = new JsonParser();
     try (FileReader reader = new FileReader(shelfLifeData)) {
-      Object obj = jsonParser.parse(reader);
-      obj = jsonParser.parse(reader);
-      obj = jsonParser.parse(reader);
-      System.out.println(obj);
-      
+      Gson gson = new Gson();
+
+      JsonObject data = (JsonObject) jsonParser.parse(reader).getAsJsonObject();
+      JsonArray sheets = data.getAsJsonArray("sheets");
+      JsonObject productList = sheets.get(2).getAsJsonObject();
+      JsonArray productListData = productList.getAsJsonArray("data");
+
+      for (int i = 0; i < productListData.size(); i++) {
+        JsonArray product = productListData.get(i).getAsJsonArray();
+        JsonObject nameObject = product.get(2).getAsJsonObject();
+        String productName = nameObject.get("Name").getAsString();
+        if (productName.equals(itemName)) {
+          findTime(product);
+        }
+      }
     } catch (FileNotFoundException e) {
       System.out.println("file not found");
     } catch (IOException e) {
       System.out.println("IOException");
     }
+
+    return "";
+  }
+
+  private int findTime(JsonArray product) {
+    Gson gson = new Gson();
+
+    for (int i = 5; i < product.size(); i++){
+      JsonObject productTimeElement = product.get(i).getAsJsonObject();
+      String json = gson.toJson(productTimeElement);
+      String[] keyValuePair = json.split(":");
+
+      if (keyValuePair.length == 2) {
+        String value = keyval[1];
+        value = value.substring(0, value.length()-1);
+        System.out.println(value);
+      }
+    }
+    return 0;
   }
 }
--- src/test/java/com/google/edith/ShelfDataReaderTest.java
+++ src/test/java/com/google/edith/ShelfDataReaderTest.java
@@ -1,5 +1,24 @@
 package com.google.edith;
 
+import java.io.IOException;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
 public class ShelfDataReaderTest {
   
+  private ShelfDataReader reader;
+
+  @Before
+  public void setUp() {
+    reader = new ShelfDataReader();
+  }
+  @Test
+  public void doesParse() {
+    reader.readFile("Lamb");
+    Assert.assertEquals("", "");
+  }
 }
