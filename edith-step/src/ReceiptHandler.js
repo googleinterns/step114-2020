e8b85154b9f5cbe4944600474f4f5e4395d6d42c
Livia Seibert <livia.seibert@duke.edu>
2020-07-15 16:33:56 +0000

    can edit form

--- edith-step/src/ReceiptHandler.js
+++ edith-step/src/ReceiptHandler.js
@@ -7,6 +7,8 @@ export default class ReceiptHandler extends React.Component {
     super(props);
     this.state = { items: [] };
     this.getReceiptData = this.getReceiptData.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    //this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
@@ -28,34 +30,87 @@ export default class ReceiptHandler extends React.Component {
     console.log(this.state.items);
   }
 
+  handleChange(i, e, property) {
+    let itemsList = [...this.state.items];
+    itemsList[i].property = event.target.value;
+    console.log(itemsList[i].property);
+    this.setState({ items: itemsList });
+  }
+/** 
+  handleSubmit(e) {
+    
+  }*/
+
   render() {
     return(
-      <div className="receipt-list">
-        {this.state.items.length > 0 &&
-          <List items={this.state.items} />
-        }
+      <div className="container">
+        <form /** onSubmit={this.handleSubmit}*/>
+        <div className="form-row">
+          <div className="col auto">
+            <span>Item</span>
+          </div>
+          <div className="col auto">
+            <span>Price</span>
+          </div>
+          <div className="col auto">
+            <span>Quantity</span>
+          </div>
+        </div>
+        {this.state.items.map((item, i) => (
+        <div className="form-row" key={i}>
+          <div className="col auto">
+            <input type="text" defaultValue={item.name} onChange={this.handleChange.bind(this, i, "name")}/>
+          </div>
+          <div className="col auto">
+            <input type="number" defaultValue={item.price} onChange={this.handleChange.bind(this, i, "price")}/>
+          </div>
+          <div className="col auto">
+            <input type="number" defaultValue={item.quantity} onChange={this.handleChange.bind(this, i, "quantity")}/>
+          </div>
+        </div>
+        ))}
+        <button className="btn btn-primary"
+              id="submit"
+              type="submit" 
+              value="Submit">Submit</button>
+        </form>
       </div>
     );
   }
 
 }
-
+/** 
 const List = (props) => {
   return (
     <form>
-      {props.items.map(item => (
-        <div className="form-row">
+      <div className="form-row">
+        <div className="col auto">
+          <span>Item</span>
+        </div>
+        <div className="col auto">
+          <span>Price</span>
+        </div>
+        <div className="col auto">
+          <span>Quantity</span>
+        </div>
+      </div>
+      {props.items.map((item, i) => (
+        <div className="form-row" key={i}>
           <div className="col auto">
-            <input type="text" defaultValue={item.name}/>
+            <input type="text" defaultValue={item.name} value={this.state.items[i].name} onChange={this.handleChange.bind(this, i, name)}/>
           </div>
           <div className="col auto">
-            <input type="number" defaultValue={item.price}/>
+            <input type="number" defaultValue={item.price} value={this.state.items[i].price} onChange={this.handleChange.bind(this, i, price)}/>
           </div>
           <div className="col auto">
-            <input type="number" defaultValue={item.quantity}/>
+            <input type="number" defaultValue={item.quantity} value={this.state.items[i].quantity} onChange={this.handleChange.bind(this, i, quantity)}/>
           </div>
         </div>
       ))}
+      <button className="btn btn-primary"
+              id="submit"
+              type="submit" 
+              value="Submit">Submit</button>
     </form>
   );
-}
+}*/
