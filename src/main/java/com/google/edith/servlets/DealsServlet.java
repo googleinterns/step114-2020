b55ee8f6efb37ccea5ecaa6c307d1793248fb30b
Livia Seibert <livia.seibert@duke.edu>
2020-07-16 19:52:42 +0000

    changed to reflect fixes in earlier PRs

--- src/main/java/com/google/edith/servlets/DealsServlet.java
+++ src/main/java/com/google/edith/servlets/DealsServlet.java
@@ -1,4 +1,4 @@
-package com.google.edith.servlets;
+/**package com.google.edith.servlets;
 
 import com.google.edith.DealItem;
 import com.google.edith.GroceryDataReader;
@@ -12,10 +12,11 @@ import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponse;*/
 
 /** Servlet that takes the user input from the receipt
   * form and uses it to retrieve the best deal. */
+  /**
 @WebServlet("/receipt-deals")
 public class DealsServlet extends HttpServlet {
 
@@ -47,6 +48,56 @@ public class DealsServlet extends HttpServlet {
     else {
       Gson gson = new Gson();
       String responseJson = gson.toJson(bestItem);
+      response.setContentType("application/json");
+      response.getWriter().println(responseJson);
+    }
+  }
+}*/
+
+package com.google.edith;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.io.BufferedReader;
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/** 
+  * Servlet that takes the user input from the receipt
+  * form and uses it to retrieve the best deal. 
+  */
+@WebServlet("/receipt-deals")
+public class DealsServlet extends HttpServlet {
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    StringBuilder stringBuilder = new StringBuilder();
+    try (BufferedReader reader = request.getReader()) {
+      String line;
+      while ((line = reader.readLine()) != null) {
+        stringBuilder.append(line).append('\n');
+      }
+    }
+
+    String receiptData = stringBuilder.toString();
+    JsonParser parser = new JsonParser();
+    JsonObject json = (JsonObject) parser.parse(receiptData);
+    String itemName = json.get("itemName").getAsString();
+
+    GroceryDataReader groceryReader = new GroceryDataReader();
+    DealItem cheapestItem = groceryReader.readFile(itemName);
+
+    if (cheapestItem == null) {
+      response.setContentType("text/plain");
+      response.getWriter().println("no deal found");
+    }
+    else {
+      Gson gson = new Gson();
+      String responseJson = gson.toJson(cheapestItem);
       response.setContentType("application/json");
       response.getWriter().println(responseJson);
     }
