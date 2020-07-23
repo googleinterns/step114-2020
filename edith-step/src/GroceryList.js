2022dfbba68f1a332db5fffee69e878d643f943c
Livia Seibert <livia.seibert@duke.edu>
2020-07-23 00:23:25 +0000

    more

---
+++ edith-step/src/GroceryList.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import axios from 'axios';
+
+export default class GroceryList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.setState({ items: [] });
+  }
+
+  componentDidMount() {
+    this.getItemData();
+  }
+
+  async getItemData() {
+    const response = await axios({
+      method: 'get',
+      url: '/grocery-list-query',
+      responseType: 'json'
+    });
+    const itemsList = response.data;
+    this.setState({ items: itemsList });
+  }
+
+  render() {
+    return (
+      <div className="container">
+      {this.state.items.length > 0 &&
+        <div className="row">
+          <div className="col-lg-2">
+            <span>Item</span>
+          </div>
+        </div>
+        <>
+        {this.state.items.map((item) => (
+        <div className="row">
+          <div className="col-lg-2">
+            <span>{item.name}</span>
+          </div>
+        </div>
+        ))}
+        </>
+      }
+      </div>
+    );
+  }
+}
