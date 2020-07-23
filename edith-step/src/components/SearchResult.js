9cbc3b46ceb4819dafc7787b7efb3cad316a1ad6
Prashant Neupane <prashantneupane8848@gmail.com>
2020-07-23 14:11:47 +0000

    Add result for items

--- edith-step/src/components/SearchResult.js
+++ edith-step/src/components/SearchResult.js
@@ -5,7 +5,7 @@ import Card from 'react-bootstrap/Card';
 const Receipt = (props) => {
   console.log(props);
   return (
-    <Card className="text-center">
+    <Card className='text-center'>
       <Card.Header>Receipt Name: {props.name}</Card.Header>
       <Card.Link href={props.fileUrl}>View Receipt Image</Card.Link>
       <Card.Body>
@@ -18,11 +18,30 @@ const Receipt = (props) => {
   );
 }
 
+const Item = (props) => {
+  console.log(props);
+  return (
+    <Card className='text-center'>
+      <Card.Body>
+        <Card.Text>
+          Name: {props.name}
+          Price: {props.price}
+          Quantity: {props.quantity}
+          Category: {props.category}
+          Expire Date: {props.expireDate}
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
+
 class SearchResult extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      showingItem: []
+      kind: 'unknown',
+      receipts: [],
+      items: []
     };
   };
 
@@ -35,14 +54,14 @@ class SearchResult extends React.Component {
         .then((response) => response.json())
         .then((entities) => {
           if (entities === undefined || entities.length == 0) {
-            console.log("there is nothing");
+            console.log('there is nothing');
           } else {
             // Item entity does not have items field.
             if (typeof entities[0].items === 'undefined') {
-              console.log("hey I am Item");
+              this.setState({kind: 'item'});
+              this.createItems(entities);
             } else {
-              // console.log('hey i am here');
-              // console.log(entities);
+              this.setState({kind: 'receipt'});
               this.createReceipts(entities);
             }
           }
@@ -52,28 +71,45 @@ class SearchResult extends React.Component {
         });
   }
 
+  createItems(itemEntity) {
+    itemEntity.forEach((item) => {
+      console.log(item);
+      this.setState({
+        items: [... this.state.items,
+                      <Item
+                        name={item.name}
+                        price={item.price}
+                        quantity={item.quantity}
+                        category={item.category}
+                        expireDate={item.expireDate}
+                      />]
+      });
+    })
+  }
+
   createReceipts(receiptsEntity) {
     receiptsEntity.forEach((receipt) => {
       console.log(receipt);
       this.setState({
-        showingItem: [...this.state.showingItem,
+        receipts: [...this.state.receipts,
                         <Receipt
                           name={receipt.name}
                           fileUrl={receipt.fileUrl}
                           storeName={receipt.storeName}
                           totalPrice={receipt.totalPrice} />]
-      })
-
-      // this.setState(prevState => ({
-      //   showingItem: [...prevState.showingItem, <Receipt receipt />]
-      // }))
+      });
     });
   }
 
   render() {
     return (
       <>
-        {this.state.showingItem}
+        {this.state.kind === 'receipt' &&
+          this.state.receipts
+        }
+        {this.state.kind === 'item' &&
+          this.state.items
+        }
       </>
     );
   }
