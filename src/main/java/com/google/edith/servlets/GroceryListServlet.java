5dfaf98201032d63030e97b151b35e76b0795103
Livia Seibert <livia.seibert@duke.edu>
2020-07-24 16:02:20 +0000

    expired item query

---
+++ src/main/java/com/google/edith/servlets/GroceryListServlet.java
@@ -0,0 +1,20 @@
+package com.google.edith;
+
+import java.io.IOException;
+import java.util.Date;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/grocery-list-query")
+public class GroceryListServlet extends HttpServlet {
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    // call Prashant's file and get a list of receipts
+    QueryItems queryItems = new QueryItems();
+    String items = queryItems.findExpiredItems(receipt);
+    response.setContentType("application/json");
+    response.getWriter().println(items);
+  }
+}
