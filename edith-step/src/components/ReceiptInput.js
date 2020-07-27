df5058e103b457071b8450abc5386f602e9bae5e
Livia Seibert <livia.seibert@duke.edu>
2020-07-27 19:45:41 +0000

    equality

--- edith-step/src/components/ReceiptInput.js
+++ edith-step/src/components/ReceiptInput.js
@@ -63,7 +63,7 @@ export default class ReceiptInput extends React.Component {
     const newDeal = await this.getDeal(this.state.itemName,
         this.state.itemPrice, this.state.itemQuantity);
     let dealMessage;
-    if (newDeal.storeName == 'no deal found' ||
+    if (newDeal.storeName === 'no deal found' ||
         newDeal.storePrice > this.state.itemPrice) {
       dealMessage = 'no deal found';
     } else {
