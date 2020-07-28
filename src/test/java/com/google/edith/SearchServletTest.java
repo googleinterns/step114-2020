e594dffe4b145a13528c3768058bfc373a1f1fa8
Prashant Neupane <prashantneupane8848@gmail.com>
2020-07-28 17:43:55 +0000

    Add tests for get method

--- src/test/java/com/google/edith/SearchServletTest.java
+++ src/test/java/com/google/edith/SearchServletTest.java
@@ -27,7 +27,10 @@ import com.google.edith.services.SearchService;
 import com.google.edith.servlets.SearchServlet;
 import com.google.edith.servlets.Receipt;
 import com.google.edith.servlets.Item;
+import com.google.gson.Gson;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.Collections;
 import java.util.ArrayList;
 import java.util.List;
@@ -42,6 +45,7 @@ import org.mockito.MockitoAnnotations;
 
 import org.mockito.ArgumentCaptor;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -97,7 +101,7 @@ public final class SearchServletTest {
     verify(searchService, times(1)).createReceiptObjects(entities);
     verify(searchService, times(0)).createItemObjects(entities);
   }
-  
+
   // When entity kind is Receipt then createReceiptObjects method should be called.
   @Test
   public void create_ItemObjects_whenEntityKindIsItem() throws IOException {
@@ -120,5 +124,93 @@ public final class SearchServletTest {
     searchServlet.doPost(request, response);
     verify(response).sendRedirect(captor.capture());
     assertEquals("/#search-results", captor.getValue());
+  }
+
+  // Check if getWriter method is called for Receipt kind.
+  @Test
+  public void check_getWriterIsCalledForReceipt_verifiesMethodCall() throws IOException {
+    setUpPostForReceipt();
+    StringWriter stringWriter = new StringWriter();
+    PrintWriter writer = new PrintWriter(stringWriter);
+    when(response.getWriter()).thenReturn(writer);
+    searchServlet.doGet(request, response);
+    verify(response, times(1)).getWriter();
+  }
+
+  // Check if receipt response has all fields.
+  @Test
+  public void check_responseFields_containsAllReceiptFields() throws IOException {
+    setUpPostForReceipt();
+    StringWriter stringWriter = new StringWriter();
+    PrintWriter writer = new PrintWriter(stringWriter);
+    when(response.getWriter()).thenReturn(writer);
+    searchServlet.doGet(request, response);
+    String servletResponse = stringWriter.toString();
+    assertTrue(servletResponse.contains("userId"));
+    assertTrue(servletResponse.contains("storeName"));
+    assertTrue(servletResponse.contains("date"));
+    assertTrue(servletResponse.contains("name"));
+    assertTrue(servletResponse.contains("fileUrl"));
+    assertTrue(servletResponse.contains("totalPrice"));
+    assertTrue(servletResponse.contains("items"));
+  }
+
+  // Check if getWriter method is called for Item kind.
+  @Test
+  public void check_getWriterIsCalledForItem_verifiesMethodCall() throws IOException {
+    setUpPostForItem();
+    StringWriter stringWriter = new StringWriter();
+    PrintWriter writer = new PrintWriter(stringWriter);
+    when(response.getWriter()).thenReturn(writer);
+    searchServlet.doGet(request, response);
+    verify(response, times(1)).getWriter();
+  }
+
+  // Check if item response has all fields.
+  @Test
+  public void check_responseFields_containsAllItemFields() throws IOException {
+    setUpPostForItem();
+    StringWriter stringWriter = new StringWriter();
+    PrintWriter writer = new PrintWriter(stringWriter);
+    when(response.getWriter()).thenReturn(writer);
+    searchServlet.doGet(request, response);
+    String servletResponse = stringWriter.toString();
+    System.out.println(servletResponse);
+
+    assertTrue(servletResponse.contains("userId"));
+    assertTrue(servletResponse.contains("name"));
+    assertTrue(servletResponse.contains("price"));
+    assertTrue(servletResponse.contains("quantity"));
+    assertTrue(servletResponse.contains("category"));
+    assertTrue(servletResponse.contains("expireDate"));
+  }
+
+  // Sets up conditions by populating kind and receipt field of SearchServlet class.
+  private void setUpPostForReceipt() throws IOException {
+    when(request.getParameter("kind")).thenReturn("Receipt");
+    when(request.getParameter("name")).thenReturn("weekend");
+    List<Entity> entities = Collections.emptyList();
+    Item item1 = new Item("12345", "apple", 1.5f, 2, "fruit", "date");
+    Item[] items = {item1};
+    Receipt receipt1 = new Receipt("12345","kro", "unknown", "weekend", "url1", 2.5f, items);
+    Receipt[] receipts = {receipt1};
+
+    when(searchService.findEntityFromDatastore("weekend", "", "Receipt", "", ""))
+          .thenReturn(entities);
+    when(searchService.createReceiptObjects(entities)).thenReturn(receipts);
+    searchServlet.doPost(request, response);
+  }
+
+  // Sets up conditions by populating kind and item field of SearchServlet class.
+  private void setUpPostForItem() throws IOException {
+    when(request.getParameter("kind")).thenReturn("Item");
+    when(request.getParameter("name")).thenReturn("weekend");
+    List<Entity> entities = Collections.emptyList();
+    Item item1 = new Item("12345", "apple", 1.5f, 2, "fruit", "date");
+    Item[] items = {item1};
+    when(searchService.findEntityFromDatastore("apple", "", "Item", "", ""))
+          .thenReturn(entities);
+    when(searchService.createItemObjects(entities)).thenReturn(items);
+    searchServlet.doPost(request, response);
   }
 }
