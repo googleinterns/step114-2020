125d84f9cce51cc370cab10cd65dca5cf600b75c
Livia Seibert <livia.seibert@duke.edu>
2020-07-29 22:46:49 +0000

    no errors in api

--- src/main/java/com/google/edith/services/EmailService.java
+++ src/main/java/com/google/edith/services/EmailService.java
@@ -1,5 +1,6 @@
 package com.google.edith;
 
+import com.google.edith.services.LoginService;
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
@@ -14,7 +15,8 @@ import com.google.api.services.gmail.Gmail;
 import com.google.api.services.gmail.GmailScopes;
 import com.google.api.services.gmail.model.Label;
 import com.google.api.services.gmail.model.ListLabelsResponse;
-
+import javax.mail.MessagingException;
+import com.google.api.services.gmail.model.Message;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
@@ -22,26 +24,140 @@ import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
 import java.util.Collections;
 import java.util.List;
+import com.google.api.services.gmail.model.Message;
+import javax.mail.internet.MimeMessage;
+import com.google.edith.servlets.UserInfo;
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import java.util.Properties;
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import java.io.ByteArrayOutputStream;
+import org.apache.commons.codec.binary.Base64;
+import java.security.GeneralSecurityException;
 
 public class EmailService {
-  private final CreateEmail createEmail = new CreateEmail();
-  private final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+  private final UserService userService = UserServiceFactory.getUserService();
+
+  private NetHttpTransport HTTP_TRANSPORT;
+  private static final String APPLICATION_NAME = "Testing Gmail API";
+  private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
   private static final String FROM_EMAIL = "livseibert@google.com";
+  private static final String TOKENS_DIRECTORY_PATH = "tokens";
+
+  /**
+   * Global instance of the scopes required by this quickstart.
+   * If modifying these scopes, delete your previously saved tokens/ folder.
+   */
+  private static final List<String> SCOPES = Collections.singletonList(GmailScopes.GMAIL_LABELS);
+  private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
   
-  public Message sendEmail() {
-    LoginService loginService = new LoginService();
-    if (loginService.checkUserLoggedIn()) {
-      UserInfo user = loginService.createUserInfo();
-      String userEmail = user.getEmail();
-      String userId = user.getUserId();
+  public void sendEmail(String subject, String body) throws IOException, GeneralSecurityException, MessagingException {
+    if (userService.isUserLoggedIn()) {
+      User currentUser = userService.getCurrentUser();
+      String recipient = currentUser.getEmail();
+      String userId = currentUser.getUserId();
+      HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
       Gmail service = new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
         .setApplicationName(APPLICATION_NAME)
         .build();
-      
-      String subject = "test email";
-      String body = "test body";
-      MimeMessage mimeMessage = createEmail.createEmail(userEmail, FROM_EMAIL, subject, body);
-      Message messageOut = createEmail.sendMessage(service, userId, mimeMessage);
+    
+      MimeMessage mimeMessage = createEmail(recipient, FROM_EMAIL, subject, body);
+      Message messageOut = sendMessage(service, userId, mimeMessage);
     }
+  }
+
+  /**
+   * Creates an authorized Credential object.
+   * @param HTTP_TRANSPORT The network HTTP Transport.
+   * @return An authorized Credential object.
+   * @throws IOException If the credentials.json file cannot be found.
+   */
+  private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
+    // Load client secrets.
+    InputStream in = EmailService.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+    if (in == null) {
+      throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
+    }
+    GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+    // Build flow and trigger user authorization request.
+    GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+        HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+            .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+            .setAccessType("offline")
+            .build();
+    LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+    return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+  }
+
+    /**
+  * Create a MimeMessage using the parameters provided.
+  *
+  * @param to email address of the receiver
+  * @param from email address of the sender, the mailbox account
+  * @param subject subject of the email
+  * @param bodyText body text of the email
+  * @return the MimeMessage to be used to send email
+  * @throws MessagingException
+  */
+  private static MimeMessage createEmail(String to,
+      String from,
+      String subject,
+      String bodyText) throws MessagingException {
+    Properties props = new Properties();
+    Session session = Session.getDefaultInstance(props, null);
+
+    MimeMessage email = new MimeMessage(session);
+
+    email.setFrom(new InternetAddress(from));
+    email.addRecipient(javax.mail.Message.RecipientType.TO,
+      new InternetAddress(to));
+    email.setSubject(subject);
+    email.setText(bodyText);
+    return email;
+  }
+
+    /**
+   * Send an email from the user's mailbox to its recipient.
+   *
+   * @param service Authorized Gmail API instance.
+   * @param userId User's email address. The special value "me"
+   * can be used to indicate the authenticated user.
+   * @param emailContent Email to be sent.
+   * @return The sent message
+   * @throws MessagingException
+   * @throws IOException
+   */
+  private static Message sendMessage(Gmail service,
+        String userId,
+        MimeMessage emailContent)
+        throws MessagingException, IOException {
+    Message message = createMessageWithEmail(emailContent);
+    message = service.users().messages().send(userId, message).execute();
+
+    System.out.println("Message id: " + message.getId());
+    System.out.println(message.toPrettyString());
+    return message;
+  }
+
+  /**
+   * Create a message from an email.
+   *
+   * @param emailContent Email to be set to raw of message
+   * @return a message containing a base64url encoded email
+   * @throws IOException
+   * @throws MessagingException
+   */
+  public static Message createMessageWithEmail(MimeMessage emailContent)
+    throws MessagingException, IOException {
+    ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+    emailContent.writeTo(buffer);
+    byte[] bytes = buffer.toByteArray();
+    String encodedEmail = Base64.encodeBase64URLSafeString(bytes);
+    Message message = new Message();
+    message.setRaw(encodedEmail);
+    return message;
   }
 }
--- src/main/java/com/google/edith/servlets/NotificationsServlet.java
+++ src/main/java/com/google/edith/servlets/NotificationsServlet.java
@@ -1,6 +1,9 @@
 package com.google.edith.servlets;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 import com.google.edith.EmailService;
+import javax.mail.MessagingException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,8 +12,17 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/notifications")
 public class NotificationsServlet extends HttpServlet {
   private EmailService emailService = new EmailService();
+
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    emailService.sendEmail();
+    try {
+      String subject = "test subject";
+      String body = "test body";
+      emailService.sendEmail(subject, body);
+    } catch (GeneralSecurityException e) {
+      System.out.println(e.getMessage());
+    } catch (MessagingException e) {
+      System.out.println(e.getMessage());
+    }
   }
 }
