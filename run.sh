aac5e06f515590a8ece17b9ca44f13497361ed1b
livseibert5 <42747661+livseibert5@users.noreply.github.com>
2020-08-05 17:27:23 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-<<<<<<< HEAD
-=======
 mvn clean
->>>>>>> fcf4ae8975a5c332bd4e87a71858910900731524
 rm -rf src/main/webapp/src.*
 pushd edith-step; yarn build; popd;
 mvn package appengine:run
