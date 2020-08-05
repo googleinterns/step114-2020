371078aea5276100e1164ed477a566ce41d405e7
Livia Seibert <livia.seibert@duke.edu>
2020-08-05 18:44:37 +0000

    linting

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 // jest.config.js
 
 // eslint max-len: ["error", { "ignoreStrings": true }]
-const modName = '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|';
-modName += 'ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$'
 module.exports = {
   'moduleNameMapper': {
-    modName: '<rootDir>/__mocks__/fileMock.js',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|'
+    + 'ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+        '<rootDir>/__mocks__/fileMock.js',
     '\\.(scss|sass|css)$': 'identity-obj-proxy',
   },
   'verbose': true,
