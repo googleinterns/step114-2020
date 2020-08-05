fd906fbf1a970a40449dd595add5ef688353c906
Malachi Brewer <malachibre@gmail.com>
2020-08-05 22:56:38 +0000

    Linter

--- src/main/java/com/google/edith/servlets/UserStatsServlet.java
+++ src/main/java/com/google/edith/servlets/UserStatsServlet.java
@@ -6,7 +6,6 @@ import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.Query;
-import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 import com.google.gson.JsonObject;
