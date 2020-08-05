7999638a9b72b63a4e8ace5041c9d4e73c5effa7
Livia Seibert <livia.seibert@duke.edu>
2020-08-05 20:50:50 +0000

    linting

--- src/main/java/com/google/edith/servlets/DealsServlet.java
+++ src/main/java/com/google/edith/servlets/DealsServlet.java
@@ -7,8 +7,6 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
